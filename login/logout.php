3d531aaddd8082a7db2a1283861452e6a0dfdd1c
Yonatan Kassahun <yonsbreak7@gmail.com>
2026-04-06 06:29:33 +0300

    Update logout.php

--- login/logout.php
+++ login/logout.php
@@ -1,6 +1,6 @@
 <?php 
 declare(strict_types=1);
-require_once __DIR__ . '/config.php';
+require_once __DIR__ . '/../config.php';
 
 // Remove remember-me token from DB if it exists
 if (!empty($_COOKIE['remember_token'])) {
